2b5323f4ed0c5411250b6a4105ec85eaabb3bf45
Alfian Ramadani <alfianr792@gmail.com>
2025-11-21 10:04:43 +0800

    Reset main: start full version

--- config/gemini.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'api_key' => env('GEMINI_API_KEY', ''),
-    'model'   => env('GEMINI_MODEL', 'gemini-2.0-flash'),
-    'url'     => env('GEMINI_ENDPOINT_URL', 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent')
-];
